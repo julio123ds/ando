e72b16e3351fc7cc7267665f096cc7230e1153d2
julio123ds <80724081+julio123ds@users.noreply.github.com>
2024-04-28 20:48:23 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function seleccionarTarjeta(tarjeta) {
+  localStorage.setItem('tarjetaSeleccionada', tarjeta);
+  // Puedes agregar más lógica aquí, como cambiar el estilo de la tarjeta seleccionada, etc.
+}
+function seleccionarCantidad(cantidad) {
+  localStorage.setItem('tarjetaCantidad', cantidad);
+  // Puedes agregar más lógica aquí, como cambiar el estilo de la tarjeta seleccionada, etc.
+}
+function seleccionarParte(Parte) {
+  localStorage.setItem('ParteSeleccionada', Parte);
+  // Puedes agregar más lógica aquí, como cambiar el estilo de la tarjeta seleccionada, etc.
+}
+function seleccionarCard2(card2) {
+  localStorage.setItem('cardSeleccionada2', card2);
+  // Puedes agregar más lógica aquí, como cambiar el estilo de la tarjeta seleccionada, etc.
+}
